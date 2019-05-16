50060c00591246cd6a38a6254f125b5a98529bf6
akuinu <akuinumail@gmail.com>
2019-05-16 22:52:52 +0300

    minor refactoring for readability

--- content.js
+++ content.js
@@ -1,14 +1,16 @@
-let elt = document.createElement("script");
-elt.innerHTML = `var urlithing = document.querySelector('meta[property=\"og:url\"]')['content'];
-console.log(urlithing);
-let d = document.createElement('div');
-let p = document.createElement('p');
-let a = document.createElement('a');
-a.innerHTML = "↺"; 
-a.href = urlithing;
-p.appendChild(a);
-d.appendChild(p);
-document.getElementsByClassName(\"clips-edit-top-nav\")[0].appendChild(d);`;
-document.head.appendChild(elt);
-
-
+let injection = '(' + function() {
+	let urlithing = document.querySelector('meta[property="og:url"]')['content'];
+	console.log(urlithing);
+	let div = document.createElement('div');
+	let p = document.createElement('p');
+	let a = document.createElement('a');
+	a.innerHTML = '↺'; 
+	a.href = urlithing;
+	p.appendChild(a);
+	div.appendChild(p);
+	document.getElementsByClassName("clips-edit-top-nav")[0].appendChild(div);
+	} + ')();';
+let script = document.createElement("script");
+script.innerHTML = injection;
+document.head.appendChild(script);
+script.remove();
